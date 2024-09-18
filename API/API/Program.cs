d649897d1ef43002b6e3c1bbc39dd4ecea82d776
John Chappell <john.claude.cameron@gmail.com>
2024-09-18 10:21:18 -0300

    Aula dia 18

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -4,33 +4,37 @@
 // - Insomnia
 // MINIMAL APIs
 
+// API retornar dados -> GET
+// API recebendo dados -> POST
+
 // import de Java
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 List<Produto> produtos = new List<Produto>();
-produtos.Add(new Produto(){
-    Nome = "Notebook",
-    Preco = 5000,
-    Quantidade = 54
-});
-produtos.Add(new Produto(){
-    Nome = "Desktop",
-    Preco = 3500,
-    Quantidade = 150
-});
-produtos.Add(new Produto(){
-    Nome = "Monitor",
-    Preco = 1200,
-    Quantidade = 15
-});
-produtos.Add(new Produto(){
-    Nome = "Caixa de Som",
-    Preco = 650,
-    Quantidade = 70
-});
+// produtos.Add(new Produto(){
+//     Nome = "Notebook",
+//     Preco = 5000,
+//     Quantidade = 54
+// });
+// produtos.Add(new Produto(){
+//     Nome = "Desktop",
+//     Preco = 3500,
+//     Quantidade = 150
+// });
+// produtos.Add(new Produto(){
+//     Nome = "Monitor",
+//     Preco = 1200,
+//     Quantidade = 15
+// });
+// produtos.Add(new Produto(){
+//     Nome = "Caixa de Som",
+//     Preco = 650,
+//     Quantidade = 70
+// });
 
 // EndPoints - Funcionalidades
 // Request - Configurar a URL e o método/verbo HTTP
@@ -38,21 +42,34 @@ produtos.Add(new Produto(){
 app.MapGet("/", () => "API de Produtos");
 
 // GET: /produto/listar
-app.MapGet("/produto/listar", () => {
-    return Results.Ok(produtos);
+app.MapGet("/produto/listar", () => 
+{
+    // produtos.Any
+    // retorna True (not empty) or False (empty)
+    // Outra maneira é contar quantos elementos existem
+    if(produtos.Count > 0)
+        return Results.Ok(produtos);
+    
+    return Results.NotFound();
+});
+
+app.MapGet("/produto/buscar/{nome}",
+(string nome) => {
+    foreach(Produto produtoCadastrado in produtos){
+        if(produtoCadastrado.Nome == nome)
+            return Results.Ok(produtoCadastrado);
+    }
+    return Results.NotFound();
 });
 
 // POST: /produto/cadastrar
-app.MapPost("/produto/cadastrar/{nome}",
-(string nome) => {
-
-    // Criar o objeto e preencher
-    Produto produto = new Produto();
-    produto.Nome = nome;
+// "/produto/cadastrar/{nome}/{preco}/{quantidade}"
+// ruim passar informações pela URL
+app.MapPost("/produto/cadastrar/",
+([FromBody] Produto produto) => {
     // Adicionando dentro da lista
     produtos.Add(produto);
-    return Results.Ok(produtos);
-
+    return Results.Created("", produto);
 });
 
 // Criar uma funcionalidade para receber informações
