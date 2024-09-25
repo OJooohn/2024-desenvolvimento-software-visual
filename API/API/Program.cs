7238bd3cf2f49efb2d53d11567f3084f5e63c68b
John Chappell <john.claude.cameron@gmail.com>
2024-09-25 09:53:45 -0300

    Atualizacao exercicios (delete/cadastrar/atualizar)

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,11 +1,4 @@
-// Testar as APIs
-// - Rest Client - Extensão do VS Code
-// - Postman
-// - Insomnia
 // MINIMAL APIs
-
-// API retornar dados -> GET
-// API recebendo dados -> POST
 
 // import de Java
 using API.Models;
@@ -14,63 +7,74 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>();
-// produtos.Add(new Produto(){
-//     Nome = "Notebook",
-//     Preco = 5000,
-//     Quantidade = 54
-// });
-// produtos.Add(new Produto(){
-//     Nome = "Desktop",
-//     Preco = 3500,
-//     Quantidade = 150
-// });
-// produtos.Add(new Produto(){
-//     Nome = "Monitor",
-//     Preco = 1200,
-//     Quantidade = 15
-// });
-// produtos.Add(new Produto(){
-//     Nome = "Caixa de Som",
-//     Preco = 650,
-//     Quantidade = 70
-// });
+List<Produto> produtos =
+[
+    new Produto() { Nome = "Notebook", Preco = 5000, Quantidade = 54 },
+    new Produto() { Nome = "Desktop", Preco = 3500, Quantidade = 150 },
+    new Produto() { Nome = "Monitor", Preco = 1200, Quantidade = 15 },
+    new Produto() { Nome = "Caixa de Som", Preco = 650, Quantidade = 70 },
+];
 
 // EndPoints - Funcionalidades
 // Request - Configurar a URL e o método/verbo HTTP
-// Response - Retornar os dados (json/xml) e o status
+// Response - Retornar os dados (json/xml) e o código de status HTTP
 app.MapGet("/", () => "API de Produtos");
 
 // GET: /produto/listar
-app.MapGet("/produto/listar", () => 
+app.MapGet("/produto/listar", () => {
+    return Results.Ok(produtos);
+});
+
+//GET: /api/produto/buscar/{id}
+app.MapGet("/api/produto/buscar/{id}", ([FromRoute] string id) =>
 {
-    // produtos.Any
-    // retorna True (not empty) or False (empty)
-    // Outra maneira é contar quantos elementos existem
-    if(produtos.Count > 0)
-        return Results.Ok(produtos);
-    
-    return Results.NotFound();
-});
-
-app.MapGet("/produto/buscar/{nome}",
-(string nome) => {
-    foreach(Produto produtoCadastrado in produtos){
-        if(produtoCadastrado.Nome == nome)
-            return Results.Ok(produtoCadastrado);
+    Produto? produto = produtos.Find(x => x.Id == id);
+    if (produto == null) {
+        return Results.NotFound();
     }
-    return Results.NotFound();
+    return Results.Ok(produto);
 });
 
-// POST: /produto/cadastrar
-// "/produto/cadastrar/{nome}/{preco}/{quantidade}"
-// ruim passar informações pela URL
-app.MapPost("/produto/cadastrar/",
-([FromBody] Produto produto) => {
-    // Adicionando dentro da lista
+//POST: /api/produto/cadastrar/param_nome
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
+{
     produtos.Add(produto);
     return Results.Created("", produto);
 });
+
+//DELETE: /api/produto/deletar/{id}
+app.MapDelete("/api/produto/deletar/{id}", ([FromRoute] string id) =>
+{
+    Produto? produto = produtos.Find(x => x.Id == id);
+    if (produto == null) {
+        return Results.NotFound();
+    }
+    produtos.Remove(produto);
+    return Results.Ok(produto);
+});
+
+//PUT: /api/produto/alterar/{id}
+app.MapPut("/api/produto/alterar/{id}", ([FromRoute] string id, [FromBody] Produto produtoAlterado) =>
+{
+    Produto? produto = produtos.Find(x => x.Id == id);
+    if (produto == null) {
+        return Results.NotFound();
+    }
+    produto.Nome = produtoAlterado.Nome;
+    produto.Quantidade = produtoAlterado.Quantidade;
+    produto.Preco = produtoAlterado.Preco;
+    return Results.Ok(produto);
+});
+
+app.Run();
+
+// --------------------------------------------------------------------
+
+// ANOTAÇÕES
+// Testar as APIs
+// - Rest Client - Extensão do VS Code
+// - Postman
+// - Insomnia
 
 // Criar uma funcionalidade para receber informações
 // - Receber informações pela URL da requisição
@@ -82,8 +86,6 @@ app.MapPost("/produto/cadastrar/",
 // Retrieve ->    Get
 // Update   ->    Put / Patch
 // Delete   ->    Delete 
-
-app.Run();
 
 // C# - Utilização dos gets e sets
 // Produto produto = new Produto(){
